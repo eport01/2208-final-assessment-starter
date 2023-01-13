7e477705db66f7b134244437d3cc3e99883aa1dd
Emily Port <emilylport@gmail.com>
2023-01-13 11:43:41 -0700

    move memoization to tourist sites facade

--- app/facades/tourist_sites_facade.rb
+++ app/facades/tourist_sites_facade.rb
@@ -1,9 +1,9 @@
 class TouristSitesFacade 
   def self.tourist_sites(country)
-    near_country = CountryFacade.capital_info(country)
-    if near_country != nil 
-      long = near_country.longitude
-      lat = near_country.latitude
+    @_near_country = CountryFacade.capital_info(country)
+    if @_near_country != nil 
+      long = @_near_country.longitude
+      lat = @_near_country.latitude
       TouristSitesService.nearby_tourist_sites(long, lat)[:features].map do |site|
         TouristSite.new(site)
       end
